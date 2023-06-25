fe92fb6fe2f4bd05e06e812b663e50776fd8b495
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2023-06-25 14:07:47 +0800

    update ent

---
+++ datasource/ent/schema/timemixin.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"time"
+)
+
+// TimeMixin implements the ent.Mixin for sharing
+// time fields with package schemas.
+type TimeMixin struct {
+	mixin.Time
+}
+
+func (TimeMixin) Fields() []ent.Field {
+
+	return []ent.Field{
+		field.Time("created_at").
+			Immutable().
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+
+}
